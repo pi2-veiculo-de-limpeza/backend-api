ecece8d980056535fbc6fc73e5ae8c12fb50fe09
gesiel dos santos freitas <gesiel.was.f@gmail.com>
2018-10-22 13:15:05 -0300

    ajustando o método de gerar token para usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    @user = create_token_params(user_params, @user)
+    #@user = create_token_params(user_params, @user)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
+   
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
@@ -51,12 +51,12 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password)
     end
 
-    def create_token_params(api_user_params, api_user)
-      random_string = (0..7).map { ('a'..'z').to_a[rand(26)] }.join
-      api_user.secret = BCrypt::Password.create(random_string)
-      payload = { name: api_user_params[:name], email: api_user_params[:email] }
-      api_user.token = JWT.encode payload, api_user.secret, 'HS256'
-      api_user
-    end
+    #def create_token_params(api_user_params, api_user)
+    #  random_string = (0..7).map { ('a'..'z').to_a[rand(26)] }.join
+    #  api_user.secret = BCrypt::Password.create(random_string)
+    #  payload = { name: api_user_params[:name], email: api_user_params[:email] }
+    #  api_user.token = JWT.encode payload, api_user.secret, 'HS256'
+    #  api_user
+    #end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,14 @@ class User
   field :token, type: String,    default:nil
   field :password, type: String
   field :history,  type: Array,  default:nil
+  field :logged_on, type:Boolean, default:false
+
+  before_save :create_token
 
   def create_token
+  	random_string = (0..7).map { ('a'..'z').to_a[rand(26)] }.join
+    self.secret = BCrypt::Password.create(random_string)
+    payload = { name: self.name, email: self.email }
+    self.token = JWT.encode payload, self.secret, 'HS256'
   end 
 end
